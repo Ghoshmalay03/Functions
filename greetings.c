5e89c2fd4c5a62d927f05299b09ea5ded605dcd7
Malay Ghosh <150132631+Ghoshmalay03@users.noreply.github.com>
2024-04-15 03:04:25 +0530

    greetings.c

--- greetings.c
+++ greetings.c
@@ -4,11 +4,11 @@ void bonjour();
 
 int main(){                 //Function call
     char a;
-    printf("Press 'I' for Indian and 'F' for French: ");
+    printf("Press 'I' for Indian and 'F' for French: ");        //Asks the user for their native country
     scanf("%c", &a);
-    if(a == 'I'){
+    if(a == 'I'){                    //If is used for Indian
         namaste();
-    } else{
+    } else{                          //else is used for French
         bonjour();
     }
     return 0;
